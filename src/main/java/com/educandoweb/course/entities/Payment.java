4494786f26905fde1ecbc68e046433d7260da5e8
Polar0471 <rodrigoarth512@gmail.com>
2023-11-11 01:13:00 -0300

    Criando entidade Payment.java e relação one-to-one com entidade Order.java

--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -2,26 +2,19 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
 
-import com.educandoweb.course.entities.enums.OrderStatus;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_order")
-public class Order implements Serializable {
-    
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
     private static final Long serialVersionUID = 1L;
 
     @Id
@@ -29,26 +22,17 @@ public class Order implements Serializable {
     private Long id;
     private Instant moment;
 
-    private Integer orderStatus;
+    @OneToOne
+    @MapsId
+    private Order order;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
-
-    @OneToMany(mappedBy = "id.order")
-    private Set<OrderItem> items = new HashSet<>();
-
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
-
-    public Order() {
+    public Payment() {
     }
 
-    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+    public Payment(Long id, Instant moment, Order order) {
         this.id = id;
         this.moment = moment;
-        setOrderStatus(orderStatus);
-        this.client = client;
+        this.order = order;
     }
 
     public Long getId() {
@@ -67,36 +51,14 @@ public class Order implements Serializable {
         this.moment = moment;
     }
 
-    public OrderStatus getOrderStatus() {
-        return OrderStatus.valueOf(orderStatus);
+    public Order getOrder() {
+        return this.order;
     }
 
-    public void setOrderStatus(OrderStatus orderStatus) {
-        if (orderStatus != null) {
-            this.orderStatus = orderStatus.getCode();
-        }
+    public void setOrder(Order order) {
+        this.order = order;
     }
 
-    public User getClient() {
-        return this.client;
-    }
-
-    public void setClient(User client) {
-        this.client = client;
-    }
-
-    public Set<OrderItem> getItems() {
-        return items;
-    }
-
-    public Payment getPayment() {
-        return this.payment;
-    }
-
-    public void setPayment(Payment payment) {
-        this.payment = payment;
-    }
-    
     @Override 
     public int hashCode() {
         final int prime = 1;
@@ -113,7 +75,7 @@ public class Order implements Serializable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Order other = (Order) obj;
+        Payment other = (Payment) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
